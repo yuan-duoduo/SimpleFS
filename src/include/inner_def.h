33c3f5af179ef51fadf615fca982b89bbaaa363a
wjy <jywang_yatb@126.com>
2018-11-24 02:29:18 +0800

    Add stubdisk file and so on.

---
+++ src/include/inner_def.h
@@ -0,0 +1,23 @@
+#ifndef __INNER_DEF_H__
+#define __INNER_DEF_H__
+
+#include <stdio.h>
+
+#define SFS_OK 			(0)
+#define SFS_ERROR 		(-1)
+
+#define TRUE			(1)
+#define FALSE       	(0)
+
+typedef unsigned char uint8_t;
+typedef unsigned int uint16_t;
+typedef unsigned long int uint32_t;
+typedef unsigned long long int uint64_t;
+
+typedef signed char int8_t; 
+typedef signed int int16_t;
+typedef signed long int int32_t;
+typedef signed long long int int64_t;
+
+
+#endif

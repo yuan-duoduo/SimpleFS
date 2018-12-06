e002868164385dfb98f2b8791f6fe5b71d3b9642
wjy <jywang_yatb@126.com>
2018-12-07 04:31:01 +0800

    Modify some files.

--- include/stub_fuse.h
+++ include/stub_fuse.h
@@ -2,15 +2,18 @@
 #define __STUB_FUSE_H__
 
 #include <fuse.h>
+#include <fuse_common.h>
+
+
 
 /* Date: 2018-11-25
 ** 定义SFS文件系统操作集
 */
 struct fuse_operations sfsOps = {
-    .getattr    =   ou_getattr,
-    .readdir    =   ou_readdir,
-    .create     =   ou_create,
-    .unlink     =   ou_unlink,
+    .getattr    =   sfsGetattr,
+    .readdir    =   sfsReaddir,
+    .create     =   sfs_create,
+    .unlink     =   sfs_unlink,
 };
 
 

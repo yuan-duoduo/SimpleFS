96d676403e75ff965dff91a6b77be5c3d568b2df
wangjiayuan <jywang_yatb@126.com>
2020-06-07 21:48:56 +0800

    添加leveldb适配函数

---
+++ src/adapter/leveldb_adapter.cpp
@@ -0,0 +1,12 @@
+/**
+ * Leveldb adapter
+ * Leveldb provide kv store for simple nas project.
+ * Author: wangjiayuan
+ * Date: 2020-06-07
+ */
+#include "leveldb_adapter.h"
+
+void LeveldbAdapter::InitLeveldb()
+{
+
+}
---
+++ src/adapter/leveldb_adapter.h
@@ -0,0 +1,12 @@
+//
+// Created by root on 6/7/20.
+//
+
+#ifndef SIMPLE_NAS_LEVELDB_ADAPTER_H
+#define SIMPLE_NAS_LEVELDB_ADAPTER_H
+
+class LeveldbAdapter {
+    void InitLeveldb();
+};
+
+#endif //SIMPLE_NAS_LEVELDB_ADAPTER_H
